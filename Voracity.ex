e9a2a5cc03fb23d48ee959dd3ad7c148fe59fbec
Carl Schrammel <carl.schrammel@gmail.com>
2015-08-11 22:18:51 -0500

    figuring out random numbers

--- Voracity.ex
+++ Voracity.ex
@@ -1,7 +1,24 @@
+
 defmodule Voracity do
-  def start() do
-     "Game started..."
+  def print_multiple_times(n) when n <= 1 do
+    IO.write random(8)
   end
+
+  def print_multiple_times(n) do
+    IO.write random(8)
+    print_multiple_times(n - 1)
+  end
+
+  def random(n) when is_integer(n) and n > 0 do
+    :random.seed(:os.timestamp())
+    :random.uniform(n)
+  end
+
+  def start() do
+     IO.puts "Game started..."
+     IO.puts print_multiple_times(100)
+  end
+
 end
   
 IO.puts Voracity.start()
