879bd4dedcb9d27037cbdbfb08ab535a405e5ad8
Carl Schrammel <carl.schrammel@gmail.com>
2015-08-16 08:22:26 -0500

    game loop

--- voracity/lib/voracity.ex
+++ voracity/lib/voracity.ex
@@ -9,17 +9,29 @@ defmodule Voracity do
   def start() do
     IO.puts "#{IO.ANSI.clear}"
     IO.puts "#{IO.ANSI.green}Game started...#{IO.ANSI.reset}\n"
+    current = random(100)
     Stream.map(1..100, fn x -> random(8) end)  
       |> Enum.shuffle
       |> Stream.with_index
-      |> Enum.map &to_grid(elem(&1, 1), elem(&1, 0))
+      |> Enum.map &to_grid(elem(&1, 1), elem(&1, 0), current)
   end
 
   def main(args) do
-    IO.puts Voracity.start()
+    game_loop("")
   end
 
-  def to_grid(n, x) when rem(n + 1, 10) == 0, do: to_string(x) <> "\n\n"
-  def to_grid(n, x), do: to_string(x) <> "   "  
+  def game_loop(input) when input == "exit" do
+    IO.puts "Have a nice day!"
+  end
+
+  def game_loop(input) do
+    IO.puts Voracity.start()
+    input = IO.getn("Enter something:", 4)
+    game_loop(input)
+  end
+
+  def to_grid(n, x, current) when rem(n + 1, 10) == 0, do: to_string(x) <> "\n\n"
+  def to_grid(n, x, current) when n == current, do: "#{IO.ANSI.red}" <> to_string(x) <> "#{IO.ANSI.reset}   "
+  def to_grid(n, x, current), do: to_string(x) <> "   "  
 
 end
