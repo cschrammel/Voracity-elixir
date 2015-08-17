5f98be699bf134bcf3e7d7bf5082612043398c4d
Carl Schrammel <carl.schrammel@gmail.com>
2015-08-17 06:19:27 -0500

    cleaned up some uglyness

--- voracity/lib/voracity.ex
+++ voracity/lib/voracity.ex
@@ -11,9 +11,8 @@ defmodule Voracity do
   end
 
   def main(args) do
-    IO.puts "#{IO.ANSI.green}Game started...#{IO.ANSI.reset}\n"
     position = random(100)
-    game_loop("", Voracity.init_board(), position)
+    game_loop("", init_board(), position)
   end
 
   def game_loop(input, board, position) when input == "q" do
@@ -22,27 +21,25 @@ defmodule Voracity do
 
   def game_loop(input, board, position) do
     IO.puts "#{IO.ANSI.clear}"
-    grid = board 
-      |> Enum.map &to_grid(elem(&1, 1), elem(&1, 0), position)
-    IO.puts grid
+    IO.puts board |> Enum.map &to_view(&1, position)
     input = IO.getn("Enter direction (q to quit):", 1)
     position = position + 1
     game_loop(input, board, position)
   end
 
-  def to_grid(n, x, position) when rem(n + 1, 10) == 0, do: grid_entry(n, x, position) <> "\n\n"
-  def to_grid(n, x, position), do: grid_entry(n, x, position)
-
-  def grid_entry(n, x, position) do
-    entry = ""
-    if n == position do 
-      entry = entry <> "#{IO.ANSI.yellow}"
+  def to_view(t, position) do
+    value = to_string(elem(t, 0))
+    index = elem(t, 1)
+    if index == position do 
+      entry = "#{IO.ANSI.yellow}" <> value <> "#{IO.ANSI.reset}"
+    else
+      entry = value
     end
-    entry = entry <> to_string(x) 
-    if n == position do 
-      entry = entry <> "#{IO.ANSI.reset}"
+    if rem(index + 1, 10) == 0 do
+      entry <> "\n\n"
+    else
+      entry <> "   "  
     end
-    entry <> "   "
-  end  
+  end
 
 end
