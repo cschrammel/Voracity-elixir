ac2cd54d7478d79883abf8a48cfd3b721a40bc6b
Carl Schrammel <carl.schrammel@gmail.com>
2015-08-18 06:51:34 -0500

    can move up, down, left, right

--- voracity/lib/voracity.ex
+++ voracity/lib/voracity.ex
@@ -19,11 +19,32 @@ defmodule Voracity do
     IO.puts "Have a nice day!"
   end
 
+  def game_loop(input, board, position) when input == "d" do
+    position = position + elem(Enum.at(board, position + 1), 0)
+    game_loop("", board, position)
+  end
+
+  def game_loop(input, board, position) when input == "a" do
+    position = position - elem(Enum.at(board, position - 1), 0)
+    game_loop("", board, position)
+  end
+
+  def game_loop(input, board, position) when input == "w" do
+    above = elem(Enum.at(board, position - 10), 0)
+    position = position - (above * 10)
+    game_loop("", board, position)
+  end
+
+  def game_loop(input, board, position) when input == "s" do
+    below = elem(Enum.at(board, position + 10), 0)
+    position = position + (below * 10)
+    game_loop("", board, position)
+  end
+
   def game_loop(input, board, position) do
     IO.puts "#{IO.ANSI.clear}"
     IO.puts board |> Enum.map &to_view(&1, position)
     input = IO.getn("Enter direction (q to quit):", 1)
-    position = position + 1
     game_loop(input, board, position)
   end
 
@@ -31,14 +52,14 @@ defmodule Voracity do
     value = to_string(elem(t, 0))
     index = elem(t, 1)
     if index == position do 
-      entry = "#{IO.ANSI.yellow}" <> value <> "#{IO.ANSI.reset}"
+      entry = "#{IO.ANSI.yellow}#{value}#{IO.ANSI.reset}"
     else
       entry = value
     end
     if rem(index + 1, 10) == 0 do
-      entry <> "\n\n"
+      "#{entry}\n\n"
     else
-      entry <> "   "  
+      "#{entry}   "  
     end
   end
 
